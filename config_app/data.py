1def9df34c7a02355f9cc699ecdbc6d8995462d8
Sunny <sunnysaraff11@gmail.com>
2021-07-28 16:21:02 +0530

    Updated data.py

--- config_app/data.py
+++ config_app/data.py
@@ -2,10 +2,19 @@ import json
 
 config_template = {}
 
+current_configs = {}
+
+default_values = {}
+
+
+def CreateDefaultObject(template_object):
+    for key in template_object['configFields']:
+        default_values[key] = template_object['configFields'][key]['default']
+
+
 # Note on Bulk Update, Change config_template.json file
 with open("config_app\\config_template.json", "rb") as file:
     template = json.load(file)
     template_id = template['templateID']
     config_template[template_id] = template
-
-current_configs = {}
+    CreateDefaultObject(config_template[template_id])
